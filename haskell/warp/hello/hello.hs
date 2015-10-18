5d8bb11e0b4c6a62b79797b1bf57165dded94cb7
Kaizhao Zhang <zhangkaizhao@gmail.com>
2015-10-19 00:53:32 +0800

    haskell: wrap. note: scotty uses warp

---
+++ haskell/warp/hello/hello.hs
@@ -0,0 +1,10 @@
+-- http://www.yesodweb.com/book/web-application-interface
+{-# LANGUAGE OverloadedStrings #-}
+import Network.Wai
+import Network.HTTP.Types (status200)
+import Network.Wai.Handler.Warp (run)
+
+application _ respond = respond $
+  responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
+
+main = run 3000 application
